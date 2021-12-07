f310e992048cc39bbadf5c54e391af53cf1ffbd6
11yang11 <90589774+11yang11@users.noreply.github.com>
2021-12-07 16:52:03 +0800

    Update and rename 04 to 04.py

--- 04.py
+++ 04.py
@@ -1,7 +1,7 @@
 import sys
 import argparse
 from Bio import SeqIO
-for record in SeqIO.parse("/hwfssz1/ST_EARTH/P18Z10200N0160/P18Z10200N0197_temp/fuyiyang/exercise/document/human_mitochondrial.fasta", "fasta"):
+for record in SeqIO.parse("human_mitochondrial.fasta", "fasta"):
     parser = argparse.ArgumentParser()
     parser.add_argument("-strand", help="the output direction of sequence",choices=["+","-"])
     args = parser.parse_args()
