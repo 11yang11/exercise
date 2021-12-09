901bdfe7d9d68047922e9d494b80e1c37841a0e3
11yang11 <90589774+11yang11@users.noreply.github.com>
2021-12-09 09:57:55 +0800

    Update 03.py

--- 03.py
+++ 03.py
@@ -4,7 +4,11 @@ from Bio import SeqIO
 for record in SeqIO.parse("human_mitochondrial.fasta", "fasta"):
     parser = argparse.ArgumentParser()
     parser.add_argument("-g", help="gc_content of subseqence",action="store_true")
+    parser.add_argument("-start", help="start base index",required=True,type=int)
+    parser.add_argument("-end", help="end base index",required=True,type=int)
     args = parser.parse_args()
     sub_seq = record.seq[args.start-1:args.end]
     if args.g:
         print((sub_seq.count("C")+sub_seq.count("G"))/len(sub_seq))
+    else:
+        print("NO Parameter '-g'")
