8078d7b30e2d571c7071ff184a21ec0eae5fd9a4
11yang11 <90589774+11yang11@users.noreply.github.com>
2021-12-09 09:49:17 +0800

    Update 03.py

--- 03.py
+++ 03.py
@@ -5,5 +5,6 @@ for record in SeqIO.parse("human_mitochondrial.fasta", "fasta"):
     parser = argparse.ArgumentParser()
     parser.add_argument("-g", help="gc_content of subseqence",action="store_true")
     args = parser.parse_args()
+    sub_seq = record.seq[args.start-1:args.end]
     if args.g:
         print((sub_seq.count("C")+sub_seq.count("G"))/len(sub_seq))
