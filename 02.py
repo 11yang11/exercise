239c052662382cbc863d7e40e8276bb716c2be9c
11yang11 <90589774+11yang11@users.noreply.github.com>
2021-12-09 09:37:33 +0800

    Update 02.py

--- 02.py
+++ 02.py
@@ -3,8 +3,8 @@ import argparse
 from Bio import SeqIO
 for record in SeqIO.parse("human_mitochondrial.fasta", "fasta"):
     parser = argparse.ArgumentParser()
-    parser.add_argument("-start", help="start base index", default=0,type=int)
-    parser.add_argument("-end", help="end base index", default=0,type=int)
+    parser.add_argument("-start", help="start base index",required=True,type=int)
+    parser.add_argument("-end", help="end base index",required=True,type=int)
     args = parser.parse_args()
-    sub_seq = record.seq[args.start:args.end]
+    sub_seq = record.seq[args.start-1:args.end]
     print(sub_seq)
