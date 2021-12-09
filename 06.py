51b1da1f5494a2e7270538840785500718176945
11yang11 <90589774+11yang11@users.noreply.github.com>
2021-12-09 10:55:15 +0800

    Update 06.py

--- 06.py
+++ 06.py
@@ -3,6 +3,7 @@ import re
 from Bio import SeqIO
 for record in SeqIO.parse("human_mitochondrial.fasta", "fasta"):
     seq = ''.join(record.seq)
+    seq = seq.upper()
     transtable = seq.maketrans('ATCG', 'TAGC')
     complement_seq = seq.translate(transtable)
     re_com_seq = complement_seq[::-1]
