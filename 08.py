204b38ab3e37129e87c7123dc4881455009b5c5b
11yang11 <90589774+11yang11@users.noreply.github.com>
2021-12-10 14:26:39 +0800

    Update 08.py

--- 08.py
+++ 08.py
@@ -1,11 +1,22 @@
 import sys
+import argparse
 from Bio import SeqIO
+parser = argparse.ArgumentParser()
+parser.add_argument("-N",help="overlapping length between two sequences with equal length",type=int)
+args = parser.parse_args()
 for record in SeqIO.parse("human_mitochondrial.fasta", "fasta"):
     seq = ''.join(record.seq)
-    l = len(seq)
-    if l % 2 == 0:
-        n = 10
+l = len(seq)
+l = len(seq)
+n = args.N
+if args.N:
+    if l % 2 == 0 and n % 2 == 0:
+        h = (l - n) / 2
+    if l % 2 != 0 and n % 2 != 0:
+        h = (l - n) / 2
     else:
-        n = 11
-    h = (l - n) / 2
+        n = n+1
+        h = (l - n) / 2
     print(seq[:int(h + n)], '\n' * 2, seq[int(h):])
+else:
+    print("No parameter '-N'")
