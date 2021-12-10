b4d60fbd84a709b7a2b097af5d489ab43d63c4e5
11yang11 <90589774+11yang11@users.noreply.github.com>
2021-12-10 14:39:33 +0800

    Update 09.py

--- 09.py
+++ 09.py
@@ -1,17 +1,34 @@
 import sys
+import argparse
 from Bio import SeqIO
 for record in SeqIO.parse("human_mitochondrial.fasta", "fasta"):
     seq = ''.join(record.seq)
-    h2 = (l - 51) / 2
-    a = seq[:int(h2 + 50)]
-    b = seq[int(h2):]
+parser = argparse.ArgumentParser()
+parser.add_argument("-N",help="overlapping length between two sequences with equal length",type=int)
+args = parser.parse_args()
+for record in SeqIO.parse("human_mitochondrial.fasta", "fasta"):
+    seq = ''.join(record.seq)
+l = len(seq)
+n = args.N
+if args.N:
+    if l % 2 == 0 and n % 2 == 0:
+        h = (l - n) / 2
+    if l % 2 != 0 and n % 2 != 0:
+        h = (l - n) / 2
+    else:
+        n = n+1
+        h = (l - n) / 2
+    a = seq[:int(h + n)]
+    b = seq[int(h):]
     start = 0
-    while start < len(b) - 49:
-        subseq_b = b[start:(start + 50)]
+    while start < len(b)- n + 1:
+        subseq_b = b[start:(start + n)]
         start_a = 0
-        while start_a < len(a) - 49:
-            subseq_a = a[start_a:(start_a + 50)]
+        while start_a < len(a) - n + 1:
+            subseq_a = a[start_a:(start_a + n)]
             if subseq_a == subseq_b:
-                print("overlap:", subseq_a, start_a, "-", start_a + 49)
+                print("overlap:", subseq_a, start_a, "-", start_a + n - 1)
             start_a += 1
         start += 1
+else:
+    print("No parameter '-N'")
